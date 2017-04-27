11a1a6e142d8120bb9b2b8367040bbb224f4caf1
Archer Zhao <changlechangsheng@gmail.com>
2017-04-27 23:08:39 +0800

    Add uploader

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,3 +1,5 @@
 class Resume < ApplicationRecord
+  mount_uploader :attachment, AttachementUploader #Tell rails to use this uploader for this model.
+  validates :name, presence: true #Make sure the owner's name is present.
   belongs_to :job
 end
