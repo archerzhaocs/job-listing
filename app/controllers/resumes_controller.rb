9fd64dcc4bb2a11e3695addf657c9f6cb6383269
Archer Zhao <changlechangsheng@gmail.com>
2017-04-27 23:46:17 +0800

    Add resume new action

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,13 +1,30 @@
 class ResumesController < ApplicationController
-  def index
-  end
+  before_action :authenticate_user!, :only => [:new, :create]
+
 
   def new
+    @job = Job.find(params[:job_id])
+    @resume = Resume.new
   end
 
   def create
+    @job = Job.find(params[:job_id])
+    @resume = Resume.new(resume_params)
+    @resume.job = @job
+
+    if @resume.save
+      redirect_to job_path(@job), notice:"简历已成功上传!"
+    else
+      render :new
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def resume_params
+    params.require(:resume).permit(:name, :attachment)
   end
 end
--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,5 +1,5 @@
 class Resume < ApplicationRecord
-  mount_uploader :attachment, AttachementUploader #Tell rails to use this uploader for this model.
+  mount_uploader :attachment, AttachmentUploader #Tell rails to use this uploader for this model.
   validates :name, presence: true #Make sure the owner's name is present.
   validates :attachment, presence: true # Attachment must be present
   belongs_to :job
